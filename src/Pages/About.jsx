6599a9b9bfbc77216945cc9811d513c381489f91
Maria Yasmeen <mariyayasmeen000@gmail.com>
2024-09-16 04:16:41 -0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,6 @@ import Header from "../Components/Header";
 import { Helmet } from 'react-helmet-async';
 import { motion } from 'framer-motion';
 import LoadingPage from './LoadingPage';
-import MyFlexibleLayout from './MyFlexibleLayout ';
 const LOADING_SCREEN_KEY = 'showLoadingScreen';
 
 function About() {
@@ -33,14 +32,24 @@ function About() {
         <title>About - Maria Yasmeen</title>
         <meta name="description" content="Crafting code to bring my ideas to life as a Developer and designer from Islamabad." />
       </Helmet>
-      {isLoading ? <LoadingPage /> : 
+
+{isLoading ? <LoadingPage /> : 
       <>
-     <MyFlexibleLayout />
-      </>
+        <Header />
+
+        <div className="aboutcss" style={{marginLeft:"auto",  width:"210px", fontSize:"14px"}}> 
+         <motion.div
+         initial={{ opacity: 0, y: 3 }}  // Start with text below and invisible
+         animate={{ opacity: 1, y: 0 }}  // End with text at its final position and visible
+         transition={{ duration: 0.9, ease: "easeOut" }}  // Smooth transition
+         style={{ overflow: 'hidden', width: "15rem", lineHeight:"19px"}} 
+>
+  Frontend Developer from Islamabad, I transform my ideas into code, making dynamic, responsive, and interactive web designs. I am confident that Web Designing and Development can be more different and impactful through observations and experiments.
+  </motion.div>
+     </div> 
+</>
       }
-   
-    </>
-  )
-}
+      </>
+)}
 
 export default About;
