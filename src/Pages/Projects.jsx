51601c3489f1a19f6d4a2f4c3285a22c0dea63b7
Maria Yasmeen <mariyayasmeen000@gmail.com>
2024-09-16 04:18:25 -0700

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,18 +5,17 @@ import { motion } from 'framer-motion';
 import { Helmet } from 'react-helmet-async';
 import './Pages.css';
 import { easeInOut } from 'framer-motion/dom';
-import Contact from '../Components/Contact';
 
 const Projects = () => {
     const projects = [
-        { name: 'Typeathon', description: 'Speed Typing Test App | 2024' },
+        { name: 'FontBook', description: 'Google Gonts Library 2024' },
         { name: 'M.Yasmeen', description: 'Clothing Store | 2024' },
+        { name: 'Typeathon', description: 'Speed Typing Test App | 2024' },
         { name: 'merapakistan', description: 'Pakistan Travel Guide | 2024' },
-        { name: 'CrypTrack', description: 'Crypto Currency Live Tracking | 2024' },
-        { name: 'Homedashboard', description: 'Commission 2024' },
         { name: 'History book', description: 'Commission 2024' },
+        { name: 'Homedashboard', description: 'Commission 2024' },
+        { name: 'CrypTrack', description: 'Crypto Currency Live Tracking | 2024' },
         { name: 'shop bazar', description: 'Commission 2024' },
-        { name: 'PIANo boyi', description: 'Commission 2024' },
         { name: 'you know it', description: 'Commission 2024' },
         { name: 'NEWSChannel', description: 'Commission 2024' },
         { name: 'PLAin tracker', description: 'UI Project' },
@@ -26,18 +25,18 @@ const Projects = () => {
     const textVariant = {
         hidden: { 
           opacity: 0, 
-          y: 40, 
-          rotateX: 40, // Tilted from the top
-          skewY: -10,   // Bent effect
+          y: 50, 
+          rotateX: 30, // Tilted from the top
+          skewY: -1,   // Bent effect
         },
         visible: { 
           opacity: 1, 
-          y: 5, 
-    rotateX: -5, 
-    skewY: -1, 
+          y: 1, 
+    rotateX: -1, 
+    skewY: 0, 
     transition: { 
-      duration: 1, 
-      ease:"easeInOut",
+      duration: 1.4, 
+      ease:"easeOut",
     }
   }
 };
@@ -49,37 +48,51 @@ const Projects = () => {
         <title>Work - Maria Yasmeen</title>
         <meta name="description" content="Browse through my most recent work in dev and design industry." />
       </Helmet>
-          
-          
-      <div className="AppContainer">
-    <div className="flex-layout">
-      <Header />
-
-      <main className="flex-content">
-        <div className="flex-scrollable">
-        <motion.div  className='scrollable-content'
-                 style={{ display: 'inline-block' }}  >
+            <Header />
+            <div className="projectcenter" >
+         
+                <motion.div className="scrollable-content"  
+ >
                     <div className="" style={{color:"transparent"}}>
                         <p>djhdb</p>
                         <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
                         <p>dkhvdb</p>
                         <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvdb</p>
+                        <p>dkhvdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvhdb</p>
+                        <p>dkhvdb</p>
+                        <p>dkhvhdb</p>
+                        <p className='projectmargintxt'>dkhvhdb</p>
                     </div>
                     <motion.div
                  initial="hidden"
                  animate="visible"
+                 className='responsive-text'
                  variants={textVariant}
                  style={{ display: 'inline-block' }} // Maintain original CSS styling
                   >
 
                     {projects.map((project) => (
+                        <><div className="" style={{display:"flex", justifyContent:"space-evenly"}}>
+                                                <span>{project.description}</span>
+                                                <span>View Case Study</span>
+                                                </div>
+
                         <Link
                             key={project.name}
-                            to={`/work/${project.name}`}
+                            // to={`/work/${project.name}`}
                             className="project-link"
                         >
-                            {project.name} <span>{project.description}</span>
+                            {project.name} 
                         </Link>
+                        </>
                     ))}
                     </motion.div>
                      <div className="" style={{color:"transparent"}}>
@@ -89,11 +102,6 @@ const Projects = () => {
                         <p>dkhvhdb</p>
                     </div>
                 </motion.div>
-                   </div>
-      </main>
-
-            <Contact />
-            </div>
             </div>
         </>
     );
