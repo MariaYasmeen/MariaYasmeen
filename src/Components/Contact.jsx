122c096e792c94654f2a456166e3f624ad50ae9f
Maria Yasmeen <mariyayasmeen000@gmail.com>
2025-01-16 04:06:17 -0800

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,18 +4,14 @@ import { Link } from 'react-router-dom';
 const Contact = () => {
     return (
         <>
-      <footer className="flex-footer">
-        <div className="footer-containers">
-          {/* First container with two boxes, each containing two buttons */}
-          <div className="container-boxes">
-            <div className="box">
-        <div className="city" style={{ display: 'inline-block', zIndex:"100" }}>
+        <div className="ctr-3  ">
+    <div className="nav-btn" style={{position:"absolute"}}>
+      <div className="gap-1 " >
+        <div className="city">
         <div style={{fontSize:"13px"}}>Islamabad, Pakistan</div>
         <Link to="" target="_blank">mariyayasmeen000@gmail.com</Link>
     </div>
-    </div>
-    <div className="box">
-    <div className="social-links" style={{ marginLeft: "40px" }} >
+    <div className="social-links" style={{ marginLeft: "40px" }}>
   <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
     <span>04</span> Instagram
   </a>
@@ -24,16 +20,12 @@ const Contact = () => {
   </a>
 </div>
 
-    </div>  
-         </div>
-          {/* Second container with one button */}
-          <div className="container-single">
-          <div className="box">
-          <Link to="https://www.canva.com/design/DAGOL9EuRjA/H3BBr7SU6rznXRDXTkqRTA/edit" target="_blank"><span>06</span> Resume</Link>
-          </div>
-          </div>
-        </div>
-      </footer>
+    </div>
+    <div className="CV-gap ">
+    <Link to="https://www.canva.com/design/DAGOL9EuRjA/H3BBr7SU6rznXRDXTkqRTA/edit" target="_blank"><span>06</span> Resume</Link>
+    </div>
+    </div>
+    </div>
         </>
     )
 };
