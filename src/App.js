e7c3d53be0c42c59f1b31b9527dbd16408ccd69f
Maria Yasmeen <mariyayasmeen000@gmail.com>
2025-02-19 15:42:12 +0500

    File code update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Projects from './Pages/Projects';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import About from './Pages/About';
+import Home from './Pages/Home';
 import Skills from './Pages/Skills';
+import Work from './Pages/Work';
+import Hacks from './Pages/Hacks';
+import "./Components/cursor.js";
+import "./Components/cursor.css";
+
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
+      <Home>
         <Routes>
-          <Route path="/" element={<About/>} />
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
-    
-          <Route path="/work" element={<Projects />} />
-           
+          <Route path="/work" element={<Work />} />
+          <Route path="/hacks" element={<Hacks />} />
         </Routes>
+        </Home>
       </BrowserRouter>
-      
     </React.StrictMode>
   );
 }
---
+++ src/Pages/Hacks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hacks() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hacks
